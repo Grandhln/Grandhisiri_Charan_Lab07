76196ea6604beb4ed234c6dfa9031fa48161de98
Lakshmi Charan Grandhisiri <grandhln@mail.uc.edu>
2026-03-02 18:01:17 -0500

    t5 done

---
+++ src/taskFive.java
@@ -0,0 +1,10 @@
+public class taskFive {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int s = 1; s <= i; s++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
